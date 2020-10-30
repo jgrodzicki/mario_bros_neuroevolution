22960f603b8255d279897bb7d6ebc2835c010634
jgrodzicki <kubagrodzicki@o2.pl>
2020-10-30 23:35:41 +0100

    Introduce presenting individual

--- individual.py
+++ individual.py
@@ -1,29 +1,28 @@
-import numpy as np
 import torch
-from typing import NamedTuple
 
 from nes_py.wrappers import JoypadSpace
 
-from network import Network, DEFAULT_NETWORK
-
-Individual = NamedTuple('Individual', (
-    ('weights', np.ndarray),
-))
+from network import Network, device
 
 
-def present_individual(
-    env: JoypadSpace,
-    individual: Individual,
-) -> None:
-    network: Network = DEFAULT_NETWORK
-    network.set_weights(individual)
+class Individual:
 
-    done = True
-    for step in range(5000):
-        if done:
-            state = env.reset()
-        action = network.forward(torch.Tensor(state.copy()).cuda())
-        state, reward, done, info = env.step(action)
-        env.render()
+    def __init__(self, weights: torch.Tensor):
+        self.weights = weights
 
-    env.close()
+    def present_individual(
+        self,
+        env: JoypadSpace,
+        network: Network,
+    ) -> None:
+        network.set_weights(self.weights)
+
+        done = True
+        for step in range(5000):
+            if done:
+                state = env.reset()
+            action = network.forward(torch.Tensor(state.copy()).to(device))
+            state, reward, done, info = env.step(action)
+            env.render()
+
+        env.close()
